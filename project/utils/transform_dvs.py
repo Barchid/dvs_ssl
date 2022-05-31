122a7ac46bcd1a97b22bef13e414c1b74bd78ff3
Sami Barchid <sami.barchid@univ-lille.fr>
2022-05-31 22:24:51 +0200

    pute

--- project/utils/transform_dvs.py
+++ project/utils/transform_dvs.py
@@ -194,7 +194,7 @@ class BackgroundActivityNoise:
 def get_frame_representation(sensor_size, timesteps):
     return transforms.Compose([
         # ToFrame(sensor_size=sensor_size, n_time_bins=timesteps),
-        ToFrame(sensor_size=sensor_size, event_count=2500),
+        ToFrame(sensor_size=sensor_size, event_count=2000),
         TakeFrames(timesteps=timesteps),
         # transforms.Lambda(lambda x: (x > 0).astype(np.float32)),
         # transforms.Lambda(lambda x: torch.from_numpy(x))
