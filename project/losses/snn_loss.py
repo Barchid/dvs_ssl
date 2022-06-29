205586ec17744a8041397c90b9ef18e1e858a1e5
Sami Barchid <sami.barchid@univ-lille.fr>
2022-06-29 20:48:12 +0200

    snn loss

--- project/losses/snn_loss.py
+++ project/losses/snn_loss.py
@@ -28,4 +28,53 @@ def emd_loss(a: torch.Tensor, b: torch.Tensor):
     # sum is the result
     return diff.sum()
 
-def 
+
+
+
+class SnnLoss(nn.Module):
+
+    def __init__(self, invariance_loss_weight: float = 25., variance_loss_weight: float = 25., covariance_loss_weight: float = 1., invariance_mode="emd", variance_mode="n", covariance_mode = "n"):
+        super(SnnLoss, self).__init__()
+        self.invariance_loss_weight = invariance_loss_weight
+        self.variance_loss_weight = variance_loss_weight
+        self.covariance_loss_weight = covariance_loss_weight
+        
+        self.invariance_loss = emd_loss if invariance_mode is "emd" else F.mse_loss
+        self.variance_mode = variance_mode
+        self.covariance_mode = covariance_mode
+
+    def forward(self, Z_a, Z_b):
+        assert Z_a.shape == Z_b.shape and len(Z_a.shape) == 3 # ensure (T,B,C)
+
+        # invariance loss
+        loss_inv = self.invariance_loss(Z_a, Z_b)
+
+        T = Z_a.shape[0]
+        Z_a = torch.count_nonzero(Z_a, dim=0) / T # number of spikes
+        Z_b = torch.count_nonzero(Z_b, dim=0) / T # same 
+
+        # variance loss
+        std_Z_a = torch.sqrt(Z_a.var(dim=0) + 1e-04)
+        std_Z_b = torch.sqrt(Z_b.var(dim=0) + 1e-04)
+        loss_v_a = torch.mean(F.relu(1 - std_Z_a))
+        loss_v_b = torch.mean(F.relu(1 - std_Z_b))
+        loss_var = loss_v_a + loss_v_b
+
+        # covariance loss
+        N, D = Z_a.shape
+        Z_a = Z_a - Z_a.mean(dim=0)
+        Z_b = Z_b - Z_b.mean(dim=0)
+        cov_Z_a = ((Z_a.T @ Z_a) / (N - 1)).square()  # DxD
+        cov_Z_b = ((Z_b.T @ Z_b) / (N - 1)).square()  # DxD
+        loss_c_a = (cov_Z_a.sum() - cov_Z_a.diagonal().sum()) / D
+        loss_c_b = (cov_Z_b.sum() - cov_Z_b.diagonal().sum()) / D
+        loss_cov = loss_c_a + loss_c_b
+
+        weighted_inv = loss_inv * self.invariance_loss_weight
+        weighted_var = loss_var * self.variance_loss_weight
+        weighted_cov = loss_cov * self.covariance_loss_weight
+
+        loss = weighted_inv + weighted_var + weighted_cov
+
+        return loss
+    
