de45b13310d8d392d8def37d8b1fa5d0ddf01352
Sami Barchid <sami.barchid@univ-lille.fr>
2022-09-12 10:56:22 +0200

    optuna loss

--- optuna_snnloss.py
+++ optuna_snnloss.py
@@ -13,6 +13,7 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import optuna
 
 import traceback
+from datetime import datetime
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 epochs = 1000
@@ -43,17 +44,17 @@ def objective(trial):
         module = SSLModule(
             n_classes=datamodule.num_classes,
             learning_rate=learning_rate,
-            epochs=100,
+            epochs=12,
             timesteps=timesteps,
-            ssl_loss="snn",
+            ssl_loss="snn_loss_emd",
             enc1="snn",
             enc2="snn",
             output_all=True,
         )
 
-        inv_sugg = trial.suggest_float("inv_sugg", 0.5, 25.0)
-        var_sugg = trial.suggest_float("var_sugg", 0.5, 25.0)
-        cov_sugg = trial.suggest_float("cov_sugg", 0.5, 25.0)
+        inv_sugg = trial.suggest_float("inv_sugg", 0.1, 25.0)
+        var_sugg = trial.suggest_float("var_sugg", 0.1, 25.0)
+        cov_sugg = trial.suggest_float("cov_sugg", 0.1, 25.0)
 
         module.criterion = SnnLoss(
             invariance_loss_weight=inv_sugg,
@@ -68,38 +69,41 @@ def objective(trial):
         )
 
         trainer = pl.Trainer(
-            logger=True,
+            logger=False,
             checkpoint_callback=False,
-            max_epochs=100,
+            max_epochs=12,
             gpus=1 if torch.cuda.is_available() else None,
             callbacks=[
                 online_finetuner,
-                EarlyStopping(monitor="val_loss", mode="min"),
-                PyTorchLightningPruningCallback(trial, monitor="online_val_acc"),
+                EarlyStopping(monitor="train_loss", mode="min"),
+                # PyTorchLightningPruningCallback(trial, monitor="online_val_acc"),
             ],
             precision=16,
         )
-        hyperparameters = dict(
-            invariance_loss_weight=inv_sugg,
-            variance_loss_weight=var_sugg,
-            covariance_loss_weight=cov_sugg,
-        )
-        trainer.logger.log_hyperparams(hyperparameters)
+        # hyperparameters = dict(
+        #     invariance_loss_weight=inv_sugg,
+        #     variance_loss_weight=var_sugg,
+        #     covariance_loss_weight=cov_sugg,
+        # )
+        # trainer.logger.log_hyperparams(hyperparameters)
         trainer.fit(module, datamodule=datamodule)
 
         # write in score
         report = open("report_emd_study.txt", "a")
         report.write(
-            f"{inv_sugg} {cov_sugg} {var_sugg} {trainer.callback_metrics['online_val_acc'].item()} \n"
+            f"ACC={trainer.callback_metrics['online_train_acc'].item()} VAL_ACC={trainer.callback_metrics['online_val_acc'].item()} INV={inv_sugg} COV={cov_sugg} VAR={var_sugg}\n"
         )
         report.flush()
         report.close()
 
-        return trainer.callback_metrics["online_val_acc"].item()
+        return trainer.callback_metrics["online_train_acc"].item()
     except:
-        traceback.print_exc()
+        # traceback.print_exc()
+        mess = traceback.format_exc()
         report = open('errors.txt', 'a')
-        report.write(f" ===> error ! \n")
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        report.write(f"{dt_string} ===> error ! {inv_sugg} {cov_sugg} {var_sugg}\n{mess}\n=========\n\n")
         report.flush()
         report.close()
 
@@ -108,14 +112,14 @@ if __name__ == "__main__":
     pl.seed_everything(1234)
 
     # pruner: optuna.pruners.BasePruner = optuna.pruners.MedianPruner()
-    study_name = f"snn_loss_emd_v4"
+    study_name = f"snn_loss_emd_v5"
     study = optuna.create_study(
         study_name=study_name,
         storage=f"sqlite:///{study_name}.db",
         direction="maximize",
         # pruner=pruner,
     )
-    study.optimize(objective, n_trials=100000)
+    study.optimize(objective, n_trials=1000000)
 
     print("Number of finished trials: {}".format(len(study.trials)))
 
