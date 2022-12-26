03bfa23254efe1fa46062788e189f53f3d0d29a1
Sami Barchid <sami.barchid@univ-lille.fr>
2022-12-26 23:39:06 +0100

    add dsec

--- project/datamodules/dsec.py
+++ project/datamodules/dsec.py
@@ -1,30 +1,25 @@
 import os
 import numpy as np
 from tonic.dataset import Dataset
-from tonic.download_utils import extract_archive, download_and_extract_archive
-import struct
 
 
-class DSecSSL(Dataset):
-    base_folder = "TODO"
-
-    sensor_size = (128, 128, 2)  # TODO
+class DSEC(Dataset):
+    sensor_size = (640, 480, 2)  # TODO
     dtype = np.dtype([("x", np.int16), ("y", np.int16), ("p", bool), ("t", np.int64)])
     ordering = dtype.names
 
-    def __init__(self, save_to, transform=None, target_transform=None):
-        super(DSecSSL, self).__init__(
-            save_to, transform=transform, target_transform=target_transform
+    def __init__(self, save_to="/datas/sandbox/", transform=None):
+        super(DSEC, self).__init__(
+            save_to=save_to,
+            transform=transform,
+            target_transform=None,
         )
-        file_path = os.path.join(self.location_on_system, self.base_folder)
 
-        for act_dir in os.listdir(file_path):
-            label = self.classes.index(act_dir)
-
-            for file in os.listdir(os.path.join(file_path, act_dir)):
-                if file.endswith("npy"):
-                    self.targets.append(label)
-                    self.data.append(os.path.join(file_path, act_dir, file))
+        for dir_city in os.listdir(self.location_on_system):
+            city_path = os.path.join(self.location_on_system, dir_city)
+            for entry in os.listdir(city_path):
+                if entry.endswith(".npy"):
+                    self.data.append(os.path.join(city_path, entry))
 
     def __getitem__(self, index):
         """
@@ -34,12 +29,10 @@ class DSecSSL(Dataset):
         input_path = self.data[index]
         events = np.load(input_path)
 
-        target = self.targets[index]
         if self.transform is not None:
             events = self.transform(events)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return events, target
+
+        return events
 
     def __len__(self):
         return len(self.data)
