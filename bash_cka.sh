089afa442b8223d2cccc6c91ea8cbd96e60f2d39
Sami Barchid <sami.barchid@univ-lille.fr>
2023-02-01 17:44:15 +0100

    train for cka

---
+++ bash_cka.sh
@@ -0,0 +1,5 @@
+python fine_cka.py --src_dataset=dvsgesture experiments/cka_dvsgesture/snn.ckpt 
+python fine_cka.py --src_dataset=dvsgesture experiments/cka_dvsgesture/cnn.ckpt 
+python fine_cka.py --src_dataset=dvsgesture experiments/cka_dvsgesture/3dcnn.ckpt 
+python fine_cka.py --src_dataset=dvsgesture experiments/cka_dvsgesture/snn_diffe_cnn.ckpt 
+python fine_cka.py --src_dataset=dvsgesture experiments/cka_dvsgesture/snn_diffe_3dcnn.ckpt 
