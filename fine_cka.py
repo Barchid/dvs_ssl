67247ca815a21a4ed06e0f43907b460a52f1c4ed
Sami Barchid <sami.barchid@univ-lille.fr>
2023-02-14 00:41:45 +0100

    go

--- fine_cka.py
+++ fine_cka.py
@@ -119,7 +119,10 @@ def main(args):
 
         module.encoder.requires_grad_(False)
 
-    name = f"{src_dataset}_{dest_dataset}_{modu.enc1}_{modu.enc2}"
+    if use_enc2:
+        name = f"{src_dataset}_{dest_dataset}_{modu.enc2}_{modu.enc1}"
+    else:
+        name = f"{src_dataset}_{dest_dataset}_{modu.enc1}_{modu.enc2}"
     for tr in trans:
         name += f"_{tr}"
 
