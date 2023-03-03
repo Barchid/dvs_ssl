416e9411fee99b4f8f4063f0ac7ddb277106be2c
Sami Barchid <sami.barchid@univ-lille.fr>
2023-03-03 02:54:11 +0100

    truc

--- fine.py
+++ fine.py
@@ -16,7 +16,7 @@ import traceback
 from datetime import datetime
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-epochs = 200 #500
+epochs = 300
 learning_rate = 3e-3  # barlowsnn=0.1, vicregsnn=0.01, dvs=1e-3
 timesteps = 12
 batch_size = 128
